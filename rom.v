d3042af7b116ad655a6c28289fa45fee9a23cca2
xiupos <me@xiupos.net>
2022-03-19 23:04:38 +0900

    blink

--- rom.v
+++ rom.v
@@ -5,10 +5,10 @@ module Rom (addr, data);
   function [3:0] rom_out;
     input [1:0] addr;
     case (addr)
-      2'b00: rom_out = 4'b0100;
-      2'b01: rom_out = 4'b0101;
-      2'b10: rom_out = 4'b0111;
-      2'b11: rom_out = 4'b0110;
+      2'b00: rom_out = 4'b0101;
+      2'b01: rom_out = 4'b0000;
+      2'b10: rom_out = 4'b0100;
+      2'b11: rom_out = 4'b1000;
       default: rom_out = 4'hx;
     endcase
   endfunction
